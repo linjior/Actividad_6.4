27bb28f46eecec585ace3b6a51553bdc2ea558c8
linjior <linjior@gmail.com>
2021-12-15 00:09:33 -0600

    Subir Proyecto final

---
+++ src/programas/Aplicacion.java
@@ -0,0 +1,15 @@
+
+package programas;
+
+import Clases.VentanaPrincipal;
+
+public class Aplicacion {
+
+    public static void main(String[] args) {
+        
+        VentanaPrincipal miVentanaPrincipal=new VentanaPrincipal();
+        miVentanaPrincipal.setVisible(true);
+        
+    }
+    
+}
